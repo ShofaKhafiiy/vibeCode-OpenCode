7ea5b7f4d204ff7c38465343a2d25106a203e189
SHOFA <shofakhafidin@gmail.com>
2026-05-30 20:57:14 +0700

    Auto-init database tables on Vercel serverless start

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
+const initDatabase = require('../src/config/init-db');
 const app = require('../src/app');
+
+initDatabase().catch(err => {
+  console.error('Failed to initialize database:', err.message);
+});
 
 module.exports = app;
